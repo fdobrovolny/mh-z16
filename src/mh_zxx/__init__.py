306359dbdfba81a3c3bc1e2c7aef4734b39335b0
Filip Dobrovolny <dobrovolny.filip@gmail.com>
2023-05-10 19:59:20 +0200

    Fixes __init__.py imports

--- src/mh_zxx/__init__.py
+++ src/mh_zxx/__init__.py
@@ -1,5 +1,5 @@
-from mh_z16.base import CO2Sensor
-from mh_z16.version import __version__
+from mh_zxx.base import CO2Sensor
+from mh_zxx.version import __version__
 
 
 __all__ = ["CO2Sensor", "__version__"]
